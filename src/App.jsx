aa562626600b7e4f475676b2bd35574715e91343
sebastian yabiku <sby2710@gmail.com>
2022-02-09 19:15:38 -0500

    se agrego plantilla privada y publica

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,8 @@
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect,
-} from "react-router-dom";
-import React, { useState } from "react";
-
-import { Layout, Menu, Tabs } from "antd";
-import {
-  MenuUnfoldOutlined,
-  MenuFoldOutlined,
-  ReadOutlined,
-  AppstoreAddOutlined,
-  UserOutlined,
-} from "@ant-design/icons";
-
-import { Header } from "./components/header/header";
+import { useState } from "react";
+import { BrowserRouter as Router, Switch } from "react-router-dom";
 import { Main } from "./components/main/main";
-
-import { PageLogin } from "./pages/login/login";
-import { PageCourses } from "./pages/courses/courses";
-import { PageStore } from "./pages/store/store";
-import { PageUser } from "./pages/user/user";
-import { Link } from "react-router-dom";
+import { LayoutPrivate } from "./layouts/layout-private/layout-private";
+import { LayoutPublic } from "./layouts/layout-public/layout-public";
 import "antd/dist/antd.css";
 import "./assets/style/main.scss";
 
@@ -30,88 +10,16 @@ import "./assets/style/main.scss";
 COMPONENTES WRAPPER
 */
 export function App() {
-  const { Header, Sider, Content } = Layout;
-  const state = {
-    collapsed: true,
-  };
-
-  const Toggle = () => {
-    useState({
-      collapsed: !state.collapsed,
-    });
-  };
-
-  const [Autorizado,SetAutorizado] = useState(false);
+  const [isLogin, setIsLogin] = useState(false);
 
   return (
     <Router>
       <div className="wrapper">
-      <Switch>
-        <Main>
-          <Layout style={{ minHeight: "100vh" }}>
-            <Sider trigger={null} collapsible collapsed={useState.collapsed}>
-              <div className="logo" />
-              <div
-                className="flex-column navBar"
-                style={{ minHeight: "100vh" }}
-              >
-                <Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
-                  <Menu.Item key="1" icon={<ReadOutlined />}>
-                    <Link to="/courses" autorizado={Autorizado} >Mis cursos</Link>
-                  </Menu.Item>
-                  <Menu.Item key="2" icon={<AppstoreAddOutlined />}>
-                    <Link to="/store" autorizado={Autorizado} >Libreria</Link>
-                  </Menu.Item>
-                  <Menu.Item key="3" icon={<UserOutlined />}>
-                    <Link to="/user" autorizado={Autorizado} >Mi perfil</Link>
-                  </Menu.Item>
-                </Menu>
-                {/* <Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
-                  <Menu.Item key="4" icon={<UserOutlined />}>
-                    Salir
-                  </Menu.Item>
-                </Menu> */}
-              </div>
-            </Sider>
-            <Layout className="site-layout">
-              <Header className="site-layout-background" style={{ padding: 0 }}>
-                {React.createElement(
-                  useState.collapsed ? MenuUnfoldOutlined : MenuFoldOutlined,
-                  {
-                    className: "trigger",
-                    onClick: Toggle,
-                  }
-                )}
-                Mis Cursos
-              </Header>
-              <Content
-                className="site-layout-background"
-                style={{
-                  margin: "24px 16px",
-                  padding: 24,
-                  minHeight: 280,
-                }}
-              >
-                Step/bar card
-                <div>
-                    <Route path="/login">
-                      <PageLogin SetAutorizado={SetAutorizado}/>
-                    </Route>
-                    <Route path="/courses">
-                      <PageCourses autorizado={Autorizado}/>
-                    </Route>
-                    <Route path="/store">
-                      <PageStore autorizado={Autorizado}/>
-                    </Route>
-                    <Route path="/user">
-                      <PageUser autorizado={Autorizado}/>
-                    </Route>
-                    <Redirect from="/" to="/login"></Redirect>
-                </div>
-              </Content>
-            </Layout>
-          </Layout>
-          {/* <div className="navBar flex-column min-h-full space-y-[500px]">
+        <Switch>
+          <Main>
+            {isLogin && <LayoutPrivate />}
+            <LayoutPublic />
+            {/* <div className="navBar flex-column min-h-full space-y-[500px]">
             <div>
               <Menu
                 onClick={handleClick}
@@ -148,7 +56,7 @@ export function App() {
               </Menu>
             </div>
           </div> */}
-        </Main>
+          </Main>
         </Switch>
       </div>
     </Router>
