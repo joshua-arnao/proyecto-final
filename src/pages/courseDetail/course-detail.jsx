950ff6ae826307f9a557a000f7c93f28a72fc8d7
Joshua Arnao Canessa <joshua@Joshuas-MacBook.local>
2022-02-15 20:02:32 -0500

    actualización detalle de cursos

--- src/pages/courseDetail/course-detail.jsx
+++ src/pages/courseDetail/course-detail.jsx
@@ -1,12 +1,39 @@
+import { Button } from "antd";
+import { LeftOutlined } from "@ant-design/icons";
+
 export function PageCourseDetail() {
-    return (
-        <div className="Page-Course-Detail">
-            <h1>TITULO DEL CURSO</h1>
+  return (
+    <div className="Page-Course-Detail">
+      <Button className="flex align-middle mb-8" type="link" icon={<LeftOutlined />}>
+        Regresar
+      </Button>
+      <div>
+        <div className="flex">
+          <figure>
+            <img className="w-full" src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png" />
+          </figure>
+          <div>
             <div>
-                Imagen:
-                <img src="" alt="" />
+              <h1>¿Cómo desarrollar una cultura de aprendizaje en remoto?</h1>
+              <p>
+                Aprendiendo a desarrollar una cultura de aprendizaje y mejora
+                continua como equipo en un contexto...
+              </p>
             </div>
-            <p>DESCRIPCIÓN DEL CURSO</p>
+            <Button
+              className="flex align-middle buttonPrimary"
+              type="primary"
+            >
+              Comprar
+            </Button>
+          </div>
         </div>
-    )
+      </div>
+
+      <div>
+        Imagen:
+        <img src="" alt="" />
+      </div>
+    </div>
+  );
 }
