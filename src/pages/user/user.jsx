d2b95f65635544a071a0d44c28bb72f21f4dfb08
Juan Diego <jdalbinoramos@gmail.com>
2022-02-15 22:31:24 -0500

    loading de antd

--- src/pages/user/user.jsx
+++ src/pages/user/user.jsx
@@ -4,10 +4,7 @@ import axios from "axios";
 import "./user.css";
 import { useSelector } from "react-redux";
 import { useHistory } from "react-router";
-import { Spin } from 'antd';
-import { LoadingOutlined } from '@ant-design/icons';
-
-const antIcon = <LoadingOutlined style={{ fontSize: 24 }} spin />;
+import { Spin} from 'antd';
 
 export function PageUser() {
   return (
@@ -112,7 +109,8 @@ function RegistrationForm() {
 
   //conditional rendering only if axios get request was loaded.
   return !isValueLoded ? (
-    <Spin indicator={antIcon} style={{marginLeft:"40%",marginRight:"40%",marginTop:"10%"}}/>
+    <Spin tip="Cargando..." style={{marginLeft:"40%",marginRight:"40%",marginTop:"10%"}}>
+  </Spin>
   ) : (
     <Form
       {...formItemLayout}
